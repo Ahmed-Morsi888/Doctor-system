f0d3b8bdda7eb413c1e6c6fde1ef7585b89150c9
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2026-01-02 14:44:30 +0200

    Add PrimeNG and PrimeUI dependencies, update styles and app configuration
    - Added `primeng` and `@primeuix/themes` to `package.json` and `package-lock.json`.
    - Integrated `tailwindcss-primeui` for enhanced styling.
    - Updated global styles to include `tailwindcss-primeui`.
    - Configured PrimeNG in `app.config.ts` with custom theme settings.
    - Modified `app.html` to replace placeholder content with a styled header and button.
    - Updated `app.ts` to import `ButtonModule` from PrimeNG for button functionality.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,40 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+
+    providePrimeNG({
+      theme: {
+        preset: 'my',
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false,
+          overlayAppendTo: 'body',
+          zIndex: {
+            modal: 1100,    // dialog, sidebar
+            overlay: 1000,  // dropdown, overlaypanel
+            menu: 1000,     // overlay menus
+            tooltip: 1100   // tooltip
+          },
+          options: {
+            cssLayer: {
+              name: 'primeng',
+              order: 'app-styles, primeng, another-css-library'
+            }
+          }
+
+        }
+      },
+    }),
   ]
 };
